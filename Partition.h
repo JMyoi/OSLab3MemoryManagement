a5d1ae6070e6263e9d4e43d5669f4ea91dfea252
Jay Chen <jaychenjjc@gmal.com>
2023-10-25 18:04:58 -0400

    template

---
+++ Partition.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Partition{
+    public:
+
+    private:
+
+
+};
--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 int main(){
 
-
     cout<<"hello world"<<endl;
     bool stop = false;
     while(!stop){
